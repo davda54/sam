0fde97713c4b86928f1b90f43f807433e93cdb18
David Samuel <david.samuel@seznam.cz>
2020-10-28 17:03:53 +0100

    make the smaller (and faster) WRN the default one

--- example/train.py
+++ example/train.py
@@ -14,7 +14,7 @@ from sam import SAM
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=128, type=int, help="Batch size used in the training and validation loop.")
-    parser.add_argument("--depth", default=28, type=int, help="Number of layers.")
+    parser.add_argument("--depth", default=16, type=int, help="Number of layers.")
     parser.add_argument("--dropout", default=0.0, type=float, help="Dropout rate.")
     parser.add_argument("--epochs", default=200, type=int, help="Total number of epochs.")
     parser.add_argument("--label_smoothing", default=0.1, type=float, help="Use 0.0 for no label smoothing.")
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--threads", default=2, type=int, help="Number of CPU threads for dataloaders.")
     parser.add_argument("--rho", default=0.05, type=int, help="Rho parameter for SAM.")
     parser.add_argument("--weight_decay", default=0.0005, type=float, help="L2 weight decay.")
-    parser.add_argument("--width_factor", default=10, type=int, help="How many times wider compared to normal ResNet.")
+    parser.add_argument("--width_factor", default=8, type=int, help="How many times wider compared to normal ResNet.")
     args = parser.parse_args()
 
     initialize(args, seed=42)
