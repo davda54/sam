70aaa3a7dc73df21c3f57dc822038cf72f5290ed
davda54 <david.samuel@seznam.cz>
2021-09-08 11:05:24 +0200

    add BN bypassing to the repository

---
+++ example/utility/bypass_bn.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+def disable_running_stats(model):
+    def _disable(module):
+        if isinstance(module, nn.BatchNorm2d):
+            module.backup_momentum = module.momentum
+            module.momentum = 0
+
+    model.apply(_disable)
+
+def enable_running_stats(model):
+    def _enable(module):
+        if isinstance(module, nn.BatchNorm2d) and hasattr(module, "backup_momentum"):
+            module.momentum = module.backup_momentum
+
+    model.apply(_enable)
