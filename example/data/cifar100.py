6ffeb8da8b7fd9fd37535f02e7a187c3a5665261
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-23 23:04:54 -0400

    modify the train/test data to allow for coarse classes instead of fine

--- example/data/cifar100.py
+++ example/data/cifar100.py
@@ -4,7 +4,8 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 from example.utility.cutout import Cutout
-
+from example.utility.cifar_utils import fine_labels, coarse_labels, coarse_idxs, coarse_label_map
+import numpy as np
 
 class CifarHundred:
     def __init__(self, batch_size, threads):
@@ -31,144 +32,21 @@ class CifarHundred:
             root="./data", train=False, download=True, transform=test_transform
         )
 
+        if True:
+            self.classes = fine_labels
+        else:
+            self.classes = coarse_labels
+            train_set.classes, test_set.classes = coarse_labels, coarse_labels
+            train_set.class_to_idx, test_set.class_to_idx = coarse_idxs, coarse_idxs
+            train_set.targets = list(map(coarse_label_map.get, train_set.targets))
+            test_set.targets = list(map(coarse_label_map.get, test_set.targets))
+
         self.train = torch.utils.data.DataLoader(
             train_set, batch_size=batch_size, shuffle=True, num_workers=threads
         )
         self.test = torch.utils.data.DataLoader(
             test_set, batch_size=batch_size, shuffle=False, num_workers=threads
         )
-
-        fine_labels = (
-            "apple",
-            "aquarium_fish",
-            "baby",
-            "bear",
-            "beaver",
-            "bed",
-            "bee",
-            "beetle",
-            "bicycle",
-            "bottle",
-            "bowl",
-            "boy",
-            "bridge",
-            "bus",
-            "butterfly",
-            "camel",
-            "can",
-            "castle",
-            "caterpillar",
-            "cattle",
-            "chair",
-            "chimpanzee",
-            "clock",
-            "cloud",
-            "cockroach",
-            "couch",
-            "crab",
-            "crocodile",
-            "cup",
-            "dinosaur",
-            "dolphin",
-            "elephant",
-            "flatfish",
-            "forest",
-            "fox",
-            "girl",
-            "hamster",
-            "house",
-            "kangaroo",
-            "keyboard",
-            "lamp",
-            "lawn_mower",
-            "leopard",
-            "lion",
-            "lizard",
-            "lobster",
-            "man",
-            "maple_tree",
-            "motorcycle",
-            "mountain",
-            "mouse",
-            "mushroom",
-            "oak_tree",
-            "orange",
-            "orchid",
-            "otter",
-            "palm_tree",
-            "pear",
-            "pickup_truck",
-            "pine_tree",
-            "plain",
-            "plate",
-            "poppy",
-            "porcupine",
-            "possum",
-            "rabbit",
-            "raccoon",
-            "ray",
-            "road",
-            "rocket",
-            "rose",
-            "sea",
-            "seal",
-            "shark",
-            "shrew",
-            "skunk",
-            "skyscraper",
-            "snail",
-            "snake",
-            "spider",
-            "squirrel",
-            "streetcar",
-            "sunflower",
-            "sweet_pepper",
-            "table",
-            "tank",
-            "telephone",
-            "television",
-            "tiger",
-            "tractor",
-            "train",
-            "trout",
-            "tulip",
-            "turtle",
-            "wardrobe",
-            "whale",
-            "willow_tree",
-            "wolf",
-            "woman",
-            "worm",
-        )
-
-        coarse_labels = (
-            "aquatic_mammals",
-            "fish",
-            "flowers",
-            "food_containers",
-            "fruit_and_vegetables",
-            "household_electrical_devices",
-            "household_furniture",
-            "insects",
-            "large_carnivores",
-            "large_man - made_outdoor_things",
-            "large_natural_outdoor_scenes",
-            "large_omnivores_and_herbivores",
-            "medium_mammals",
-            "non - insect_invertebrates",
-            "people",
-            "reptiles",
-            "small_mammals",
-            "trees",
-            "vehicles_1",
-            "vehicles_2",
-        )
-
-        if True:
-            self.classes = fine_labels
-        else:
-            self.classes = coarse_labels
-
     def _get_statistics(self):
         train_set = torchvision.datasets.CIFAR100(
             root="./cifar100",
