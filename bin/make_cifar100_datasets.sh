0cef36d0823c2d176afb2ad66653a94d8ddcdaf1
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-27 22:22:22 -0400

    missing var in shell scripts

--- bin/make_cifar100_datasets.sh
+++ bin/make_cifar100_datasets.sh
@@ -9,7 +9,7 @@ do
           for th in 2
           do
               CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py \
-              --${}_classes --crop_size $cs --batch_size $bs --threads $th \
+              --${class}_classes --crop_size $cs --batch_size $bs --threads $th \
               | tee output/log_dataset_CIFAR100_fineFalse_crop${cs}_batch${bs}_threads${th}.log
             done
         done
