d5c57f86999b766cf42047b3238e4d1e6e472165
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-27 21:58:29 -0400

    shell script to make datasets

--- bin/make_cifar100_datasets.sh
+++ bin/make_cifar100_datasets.sh
@@ -8,8 +8,7 @@ do
         for th in 2
         do
             CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py \
-            --coarse_classes --crop_size cs \
-            --batch_size $bs --threads $th \
+            --coarse_classes --crop_size $cs --batch_size $bs --threads $th \
             | tee output/log_dataset_CIFAR100_fineFalse_crop${cs}_batch${bs}_threads${th}.log
 
         done
