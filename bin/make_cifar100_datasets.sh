7ffc58948f144dbb51beb182b7fb2d0965f75c01
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-29 19:26:58 -0400

    lowercase in path

--- bin/make_cifar100_datasets.sh
+++ bin/make_cifar100_datasets.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 #classes=("coarse", "fine")
-crop_sizes=(4, 8, 16, 32)
-
-sc=("all")
+crop_sizes=(4 8 16 32)
+superclasses="all"
 for class in coarse; do
-  for sc in "${superclasses[@]}" in; do
+  for sc in "${superclasses[@]}"; do
     for cs in "${crop_sizes[@]}"; do
-      CUDA_VISIBLE_DEVICES=7 python -u -m src.data.cifar100 \
-      --${class}_classes --crop_size $cs --batch_size $bs --threads $th \
-      | tee output/log_dataset_${class}_class${sc}_crop${cs}_batch${bs}_threads${th}.log
+      CUDA_VISIBLE_DEVICES=7 python -u -m src.data.cifar100 --${class}_classes --crop_size $cs \
+      | tee logs/log_dataset_${class}_${sc}_crop${cs}.log
     done
   done
 done
@@ -18,9 +16,8 @@ superclasses=("aquatic_mammals", "fish", "flowers", "food_containers", "fruit_an
 for class in fine; do
   for sc in "${superclasses[@]}"; do
     for cs in "${crop_sizes[@]}"; do
-      CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py \
-      --${class}_classes --crop_size $cs --batch_size $bs --threads $th \
-      | tee output/log_dataset_${class}_class${sc}_crop${cs}_batch${bs}_threads${th}.log
+      CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py --${class}_classes --crop_size $cs \
+      | tee logs/log_dataset_${class}_${sc}_crop${cs}.log
     done
   done
 done
