972a94d23c6fbdf224ed118f6070832bc18cfa29
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-29 22:01:21 -0400

    minor changes to shell script

--- bin/make_coarse_models.sh
+++ bin/make_coarse_models.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cl="coarse"
-sc="all"
 crop_options=(4 8 16)
 depth_options=(16 20 24)
 width_options=(2 4 6 8)
@@ -10,8 +8,8 @@ for cr in "${crop_options[@]}"; do
   for dp in "${depth_options[@]}"; do
     for wd in "${width_options[@]}"; do
   CUDA_VISIBLE_DEVICES=7 python -u src/train.py \
-  --${cl}_classes --crop_size $cr --depth $dp --width_factor $wd \
-  | tee logs/model_${cl}_${sc}_crop${cr}_depth${dp}_width${wd}.log
+  --coarse_classes --crop_size $cr --depth $dp --width_factor $wd \
+  | tee logs/model_coarse_all_crop${cr}_depth${dp}_width${wd}.log
     done
   done
 done
