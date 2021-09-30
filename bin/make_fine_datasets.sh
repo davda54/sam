9839aca4d783d20795e61da2a3057de78bedfc15
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-29 21:56:47 -0400

    minor changes to shell scripts

--- bin/make_fine_datasets.sh
+++ bin/make_fine_datasets.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-crop_options=(4 8 16 32)
-super_classes=("all")
-for cl in "coarse"; do
-  for sc in "${super_classes[@]}"; do
-    for cs in "${crop_options[@]}"; do
-      CUDA_VISIBLE_DEVICES=7 python -u  src/cifar100.py --${cl}_classes --crop_size $cs \
-      | tee logs/dataset_${cl}_${sc}_crop${cs}.log
-    done
-  done
-done
-
 #super_classes=("aquatic_mammals", "fish", "flowers", "food_containers", "fruit_and_vegetables", "household_electrical_devices", "household_furniture", "insects", "large_carnivores", "large_man - made_outdoor_things", "large_natural_outdoor_scenes", "large_omnivores_and_herbivores", "medium_mammals", "non - insect_invertebrates", "people", "reptiles", "small_mammals", "trees", "vehicles_1", "vehicles_2")
 #for cl in "fine"; do
 #  for sc in "${super_classes[@]}"; do
