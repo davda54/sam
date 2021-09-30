c24f2b6a4fa817303abd8c0506fe2a394c4df10d
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-29 22:06:28 -0400

    minor changes to shell scripts

--- bin/make_coarse_datasets.sh
+++ bin/make_coarse_datasets.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-classes=("coarse")
-super_classes=("all")
 crop_options=(4 8 16 32)
 
-for cl in classes; do
-  for sc in super_classes; do
-    for cs in "${crop_options[@]}"; do
-      CUDA_VISIBLE_DEVICES=7 python -u  src/cifar100.py --${cl}_classes --crop_size $cs \
-      | tee logs/dataset_${cl}_${sc}_crop${cs}.log
-    done
-  done
+for cs in "${crop_options[@]}"; do
+  CUDA_VISIBLE_DEVICES=7 python -u  src/cifar100.py --coarse_classes --crop_size $cs \
+  | tee logs/dataset_coarse_all_crop${cs}.log
 done
