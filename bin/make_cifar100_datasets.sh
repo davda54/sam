7b4e98be95c422231d86791593ca5cfd3b21bec8
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-29 20:09:13 -0400

    comment out fine classes in make_cifar100_datasets.sh

--- bin/make_cifar100_datasets.sh
+++ bin/make_cifar100_datasets.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#classes=("coarse", "fine")
-
 crop_options=(4 8 16 32)
 super_classes=("all")
 for cl in "coarse"; do
@@ -13,12 +11,12 @@ for cl in "coarse"; do
   done
 done
 
-super_classes=("aquatic_mammals", "fish", "flowers", "food_containers", "fruit_and_vegetables", "household_electrical_devices", "household_furniture", "insects", "large_carnivores", "large_man - made_outdoor_things", "large_natural_outdoor_scenes", "large_omnivores_and_herbivores", "medium_mammals", "non - insect_invertebrates", "people", "reptiles", "small_mammals", "trees", "vehicles_1", "vehicles_2")
-for cl in "fine"; do
-  for sc in "${super_classes[@]}"; do
-    for cs in "${crop_options[@]}"; do
-      CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py --${cl}_classes --crop_size $cs \
-      | tee logs/dataset_${cl}_${sc}_crop${cs}.log
-    done
-  done
-done
+#super_classes=("aquatic_mammals", "fish", "flowers", "food_containers", "fruit_and_vegetables", "household_electrical_devices", "household_furniture", "insects", "large_carnivores", "large_man - made_outdoor_things", "large_natural_outdoor_scenes", "large_omnivores_and_herbivores", "medium_mammals", "non - insect_invertebrates", "people", "reptiles", "small_mammals", "trees", "vehicles_1", "vehicles_2")
+#for cl in "fine"; do
+#  for sc in "${super_classes[@]}"; do
+#    for cs in "${crop_options[@]}"; do
+#      CUDA_VISIBLE_DEVICES=7 python -u src/data/cifar100.py --${cl}_classes --crop_size $cs \
+#      | tee logs/dataset_${cl}_${sc}_crop${cs}.log
+#    done
+#  done
+#done
