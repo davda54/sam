8dc22b94618c33e96ac65e2b52fafce522f1a2bd
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-30 23:54:47 -0400

    add scripts for exploring the fine model training space

---
+++ bin/explore_fine_models/gpu0.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+gpu=0
+cr=16
+kr=4
+dp=16
+wd=6
+super_classes=("food_containers" "large_man-made_outdoor_things" "non-insect_invertebrates")
+
+for sc in "${super_classes[@]}"; do
+  python -u src/train.py --gpu $gpu \
+  --fine_classes --superclass $sc \
+  --crop_size $cr --kernel_size $kr \
+  --depth $dp --width_factor $wd  \
+  | tee logs/model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log
+done
---
+++ bin/explore_fine_models/gpu5.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+gpu=5
+cr=32
+kr=8
+dp=28
+wd=10
+super_classes=("food_containers" "large_man-made_outdoor_things" "non-insect_invertebrates")
+
+for sc in "${super_classes[@]}"; do
+  python -u src/train.py --gpu $gpu \
+  --fine_classes --superclass $sc \
+  --crop_size $cr --kernel_size $kr \
+  --depth $dp --width_factor $wd  \
+  | tee logs/model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log
+done
