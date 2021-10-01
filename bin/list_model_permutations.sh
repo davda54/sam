d5f3998db62a4238b5fb86d138f9e96d622a56b3
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-30 20:32:15 -0400

    new bin files

--- bin/list_model_permutations.sh
+++ bin/list_model_permutations.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+crop_options=(4 8 16)
+depth_options=(4 8 12 16 20 24)
+width_options=(2 4 6 8)
+kernel_options=(2 3 4)
+
+for cr in "${crop_options[@]}"; do
+  for kr in "${kernel_options[@]}"; do
+    for wd in "${width_options[@]}"; do
+      for dp in "${depth_options[@]}"; do
+        echo -e "model_coarse_all_crop${cr}_kernel${kr}_depth${dp}_width${wd}" >> permutations
+      done
+    done
+  done
+done
+
 crop_options=(16 32)
 depth_options=(12 16 20 24)
 width_options=(6 8 10 12)
@@ -9,21 +24,12 @@ super_classes=("aquatic_mammals" "fish" "flowers" "food_containers" "fruit_and_v
 "large_natural_outdoor_scenes" "large_omnivores_and_herbivores" "medium_mammals" "non-insect_invertebrates"
 "people" "reptiles" "small_mammals" "trees" "vehicles_1" "vehicles_2")
 
-
 for cr in "${crop_options[@]}"; do
   for kr in "${kernel_options[@]}"; do
     for dp in "${depth_options[@]}"; do
       for wd in "${width_options[@]}"; do
         for sc in "${super_classes[@]}"; do
-          python -u src/train.py \
-          --gpu 7 \
-          --fine_classes \
-          --superclass $sc
-          --crop_size $cr \
-          --kernel_size $kr \
-          --depth $dp \
-          --width_factor $wd  \
-          | tee logs/model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log
+          echo -e "model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}" >> permutations
         done
       done
     done
