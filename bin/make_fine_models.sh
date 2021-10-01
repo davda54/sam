c80722234576dd2baa5a5b40813ca57f598f1f3e
Glenn Matlin <glenn.matlin@gmail.com>
2021-09-30 23:55:08 -0400

    script modification

--- bin/make_fine_models.sh
+++ bin/make_fine_models.sh
@@ -4,11 +4,10 @@ crop_options=(16 32)
 kernel_options=(4 8)
 depth_options=(16 22 28)
 width_options=(6 8 10)
-super_classes=("food_containers" "large_man-made_outdoor_things" "non-insect_invertebrates")
-#super_classes=("aquatic_mammals" "fish" "flowers" "food_containers" "fruit_and_vegetables"
-#"household_electrical_devices" "household_furniture" "insects" "large_carnivores" "large_man-made_outdoor_things"
-#"large_natural_outdoor_scenes" "large_omnivores_and_herbivores" "medium_mammals" "non-insect_invertebrates"
-#"people" "reptiles" "small_mammals" "trees" "vehicles_1" "vehicles_2")
+super_classes=("aquatic_mammals" "fish" "flowers" "food_containers" "fruit_and_vegetables"
+"household_electrical_devices" "household_furniture" "insects" "large_carnivores" "large_man-made_outdoor_things"
+"large_natural_outdoor_scenes" "large_omnivores_and_herbivores" "medium_mammals" "non-insect_invertebrates"
+"people" "reptiles" "small_mammals" "trees" "vehicles_1" "vehicles_2")
 
 
 for i in "${!crop_options[@]}"; do
