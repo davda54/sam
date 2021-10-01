cffe6cd97a61d8235864be896c98b060db3b00d4
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-01 15:18:10 -0400

    fix issue with tee output going to root not target

--- bin/explore_fine_models/batch2/perm1.sh
+++ bin/explore_fine_models/batch2/perm1.sh
@@ -12,6 +12,5 @@ for sc in "${super_classes[@]}"; do
   --fine_classes --superclass $sc \
   --crop_size $cr --kernel_size $kr \
   --depth $dp --width_factor $wd  \
-  | tee logs/model/fine/${sc}/crop${cr}/kernel${kr}/depth${dp}/width${wd}/ \
-  model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log
+  | tee "logs/model/fine/${sc}/crop${cr}/kernel${kr}/depth${dp}/width${wd}/model_fine_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
 done
