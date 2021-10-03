f1935a45af228141a24dace7724dc0a4ffdf8c42
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-03 14:04:29 -0400

    bin scripts

--- bin/coarse_models/crop24/crop24_batch.sh
+++ bin/coarse_models/crop24/crop24_batch.sh
@@ -1,9 +1,9 @@
-tmux new-session -d -s "24_6_16_2" conda activate sam && bash bin/coarse_models/crop24/24_6_16_2.sh
-tmux new-session -d -s "24_6_16_6" conda activate sam && bash bin/coarse_models/crop24/24_6_16_6.sh
-tmux new-session -d -s "24_6_16_10" conda activate sam && bash bin/coarse_models/crop24/24_6_16_10.sh
-tmux new-session -d -s "24_6_22_2" conda activate sam && bash bin/coarse_models/crop24/24_6_22_2.sh
-tmux new-session -d -s "24_6_22_6" conda activate sam && bash bin/coarse_models/crop24/24_6_22_6.sh
-tmux new-session -d -s "24_6_22_10" conda activate sam && bash bin/coarse_models/crop24/24_6_22_10.sh
-tmux new-session -d -s "24_6_28_2" conda activate sam && bash bin/coarse_models/crop24/24_6_28_2.sh
-tmux new-session -d -s "24_6_28_6" conda activate sam && bash bin/coarse_models/crop24/24_6_28_6.sh
-tmux new-session -d -s "24_6_28_10" conda activate sam && bash bin/coarse_models/crop24/24_6_28_10.sh
+tmux new-session -d -s "24_6_16_2" bash bin/coarse_models/crop24/24_6_16_2.sh # ran
+tmux new-session -d -s "24_6_16_6" bash bin/coarse_models/crop24/24_6_16_6.sh # ran
+tmux new-session -d -s "24_6_16_10" bash bin/coarse_models/crop24/24_6_16_10.sh # ran
+tmux new-session -d -s "24_6_22_2" bash bin/coarse_models/crop24/24_6_22_2.sh # ran
+tmux new-session -d -s "24_6_22_6" bash bin/coarse_models/crop24/24_6_22_6.sh # ran
+tmux new-session -d -s "24_6_22_10" bash bin/coarse_models/crop24/24_6_22_10.sh # ran
+tmux new-session -d -s "24_6_28_2" bash bin/coarse_models/crop24/24_6_28_2.sh
+tmux new-session -d -s "24_6_28_6" bash bin/coarse_models/crop24/24_6_28_6.sh
+tmux new-session -d -s "24_6_28_10" bash bin/coarse_models/crop24/24_6_28_10.sh
