ce8758ad815e993ee91d37079eef06e5b7234c31
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 14:23:41 -0400

    redo coarse crop8 scripts

--- bin/models/coarse/crop8/8_2_28_10.sh
+++ bin/models/coarse/crop8/8_2_28_10.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-gpu=7
-crop_options=(8)
-kernel_options=(2)
-depth_options=(28) # (16 22 28)
-width_options=(10) # (2 6 10)
+cr=8
+kr=2
+dp=28
+wd=10
+gpu=0
 
-for i in "${!crop_options[@]}"; do
-    cr="${crop_options[i]}"; kr="${kernel_options[i]}"
-    for dp in "${depth_options[@]}"; do
-      for wd in "${width_options[@]}"; do
-        python -u src/train.py \
-        --gpu $gpu \
-        --coarse_classes \
-        --crop_size $cr \
-        --kernel_size $kr \
-        --depth $dp \
-        --width_factor $wd  \
-        | tee "logs/model/coarse/all/crop${cr}/kernel${kr}/depth${dp}/width${wd}/model_coarse_all_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
-    done
-  done
-done
+mkdir -p "logs/model/coarse/all/crop${cr}/kernel${kr}/depth${dp}/width${wd}/"
+python -u src/train.py --gpu $gpu \
+  --coarse_classes \
+  --crop_size $cr --kernel_size $kr \
+  --depth $dp --width_factor $wd |
+  tee "logs/model/coarse/all/crop${cr}/kernel${kr}/depth${dp}/width${wd}/model_coarse_all_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
