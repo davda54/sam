3cf8727e733ab9d8aa5d11bfc32e096f59aef521
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 14:21:33 -0400

    redo coarse crop16 scripts

--- bin/models/coarse/crop16/16_4_16_6.sh
+++ bin/models/coarse/crop16/16_4_16_6.sh
@@ -6,9 +6,9 @@ kr=4
 dp=16
 wd=6
 
-mkdir -p "logs/model/coarse/${sc}/crop${cr}/kernel${kr}/depth${dp}/width${wd}/"
+mkdir -p "logs/model/coarse/all/crop${cr}/kernel${kr}/depth${dp}/width${wd}/"
 python -u src/train.py --gpu $gpu \
   --coarse_classes \
   --crop_size $cr --kernel_size $kr \
   --depth $dp --width_factor $wd |
-  tee "logs/model/coarse/${sc}/crop${cr}/kernel${kr}/depth${dp}/width${wd}/model_coarse_${sc}_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
+  tee "logs/model/coarse/all/crop${cr}/kernel${kr}/depth${dp}/width${wd}/model_coarse_all_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
