9e6c55418c6fe8eb2c1986698a06392d1201945e
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 14:37:47 -0400

    gputil is required in setup

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -4,4 +4,5 @@ conda init bash
 conda activate sam
 conda install pytorch torchvision cudatoolkit=10.2 -c pytorch --yes
 conda install numpy scikit-learn pandas tqdm isort black --yes
+conda install -c conda-forge gputil --yes
 pip install ptflops
