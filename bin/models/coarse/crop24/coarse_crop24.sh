555f22253e8448904527ee6341a20cf51f0e921f
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 15:22:33 -0400

    change 8->6 coarse crop 24

--- bin/models/coarse/crop24/coarse_crop24.sh
+++ bin/models/coarse/crop24/coarse_crop24.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 tmux new-session -d -s "c-24-6-16-2" bash bin/models/coarse/crop24/24_6_16_2.sh
-tmux new-session -d -s "c-24-6-16-8" bash bin/models/coarse/crop24/24_6_16_8.sh
+tmux new-session -d -s "c-24-6-16-6" bash bin/models/coarse/crop24/24_6_16_6.sh
 tmux new-session -d -s "c-24-6-16-10" bash bin/models/coarse/crop24/24_6_16_10.sh
 tmux new-session -d -s "c-24-6-22-2" bash bin/models/coarse/crop24/24_6_22_2.sh
-tmux new-session -d -s "c-24-6-22-8" bash bin/models/coarse/crop24/24_6_22_8.sh
+tmux new-session -d -s "c-24-6-22-6" bash bin/models/coarse/crop24/24_6_22_6.sh
 tmux new-session -d -s "c-24-6-22-10" bash bin/models/coarse/crop24/24_6_22_10.sh
 tmux new-session -d -s "c-24-6-28-2" bash bin/models/coarse/crop24/24_6_28_2.sh
-tmux new-session -d -s "c-24-6-28-8" bash bin/models/coarse/crop24/24_6_28_8.sh
+tmux new-session -d -s "c-24-6-28-6" bash bin/models/coarse/crop24/24_6_28_6.sh
 tmux new-session -d -s "c-24-6-28-10" bash bin/models/coarse/crop24/24_6_28_10.sh
