5ce02ba6612233e59051a7e06320e9b7ed54f7c3
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 14:27:02 -0400

    coarse shell paths were wrong

--- bin/models/coarse/crop24/coarse_crop24.sh
+++ bin/models/coarse/crop24/coarse_crop24.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-tmux new-session -d -s "24_6_16_2" bash bin/coarse/crop24/24_6_16_2.sh
-tmux new-session -d -s "24_6_16_8" bash bin/coarse/crop24/24_6_16_8.sh
-tmux new-session -d -s "24_6_16_10" bash bin/coarse/crop24/24_6_16_10.sh
-tmux new-session -d -s "24_6_22_2" bash bin/coarse/crop24/24_6_22_2.sh
-tmux new-session -d -s "24_6_22_8" bash bin/coarse/crop24/24_6_22_8.sh
-tmux new-session -d -s "24_6_22_10" bash bin/coarse/crop24/24_6_22_10.sh
-tmux new-session -d -s "24_6_28_2" bash bin/coarse/crop24/24_6_28_2.sh
-tmux new-session -d -s "24_6_28_8" bash bin/coarse/crop24/24_6_28_8.sh
-tmux new-session -d -s "24_6_28_10" bash bin/coarse/crop24/24_6_28_10.sh
+tmux new-session -d -s "24_6_16_2" bash bin/models/coarse/crop24/24_6_16_2.sh
+tmux new-session -d -s "24_6_16_8" bash bin/models/coarse/crop24/24_6_16_8.sh
+tmux new-session -d -s "24_6_16_10" bash bin/models/coarse/crop24/24_6_16_10.sh
+tmux new-session -d -s "24_6_22_2" bash bin/models/coarse/crop24/24_6_22_2.sh
+tmux new-session -d -s "24_6_22_8" bash bin/models/coarse/crop24/24_6_22_8.sh
+tmux new-session -d -s "24_6_22_10" bash bin/models/coarse/crop24/24_6_22_10.sh
+tmux new-session -d -s "24_6_28_2" bash bin/models/coarse/crop24/24_6_28_2.sh
+tmux new-session -d -s "24_6_28_8" bash bin/models/coarse/crop24/24_6_28_8.sh
+tmux new-session -d -s "24_6_28_10" bash bin/models/coarse/crop24/24_6_28_10.sh
--- bin/models/coarse/crop8/coarse_crop8.sh
+++ bin/models/coarse/crop8/coarse_crop8.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-tmux new-session -d -s "8_2_16_6" bash bin/coarse/crop8/8_2_16_6.sh
-tmux new-session -d -s "8_2_16_8" bash bin/coarse/crop8/8_2_16_8.sh
-tmux new-session -d -s "8_2_16_10" bash bin/coarse/crop8/8_2_16_10.sh
-tmux new-session -d -s "8_2_22_6" bash bin/coarse/crop8/8_2_22_6.sh
-tmux new-session -d -s "8_2_22_8" bash bin/coarse/crop8/8_2_22_8.sh
-tmux new-session -d -s "8_2_22_10" bash bin/coarse/crop8/8_2_22_10.sh
-tmux new-session -d -s "8_2_28_6" bash bin/coarse/crop8/8_2_28_6.sh
-tmux new-session -d -s "8_2_28_8" bash bin/coarse/crop8/8_2_28_8.sh
-tmux new-session -d -s "8_2_28_10" bash bin/coarse/crop8/8_2_28_10.sh
+tmux new-session -d -s "8_2_16_6" bash bin/models/coarse/crop8/8_2_16_6.sh
+tmux new-session -d -s "8_2_16_8" bash bin/models/coarse/crop8/8_2_16_8.sh
+tmux new-session -d -s "8_2_16_10" bash bin/models/coarse/crop8/8_2_16_10.sh
+tmux new-session -d -s "8_2_22_6" bash bin/models/coarse/crop8/8_2_22_6.sh
+tmux new-session -d -s "8_2_22_8" bash bin/models/coarse/crop8/8_2_22_8.sh
+tmux new-session -d -s "8_2_22_10" bash bin/models/coarse/crop8/8_2_22_10.sh
+tmux new-session -d -s "8_2_28_6" bash bin/models/coarse/crop8/8_2_28_6.sh
+tmux new-session -d -s "8_2_28_8" bash bin/models/coarse/crop8/8_2_28_8.sh
+tmux new-session -d -s "8_2_28_10" bash bin/models/coarse/crop8/8_2_28_10.sh
