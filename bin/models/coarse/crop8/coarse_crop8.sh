b9232eebf16caa9f611c1449c5a42d24b38a151d
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-12 18:23:53 -0400

    8->2 in coarse crop 8

--- bin/models/coarse/crop8/coarse_crop8.sh
+++ bin/models/coarse/crop8/coarse_crop8.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+tmux new-session -d -s "c-8-2-16-2" bash bin/models/coarse/crop8/8_2_16_2.sh
 tmux new-session -d -s "c-8-2-16-6" bash bin/models/coarse/crop8/8_2_16_6.sh
-tmux new-session -d -s "c-8-2-16-8" bash bin/models/coarse/crop8/8_2_16_8.sh
 tmux new-session -d -s "c-8-2-16-10" bash bin/models/coarse/crop8/8_2_16_10.sh
+tmux new-session -d -s "c-8-2-22-2" bash bin/models/coarse/crop8/8_2_22_2.sh
 tmux new-session -d -s "c-8-2-22-6" bash bin/models/coarse/crop8/8_2_22_6.sh
-tmux new-session -d -s "c-8-2-22-8" bash bin/models/coarse/crop8/8_2_22_8.sh
 tmux new-session -d -s "c-8-2-22-10" bash bin/models/coarse/crop8/8_2_22_10.sh
+tmux new-session -d -s "c-8-2-28-2" bash bin/models/coarse/crop8/8_2_28_2.sh
 tmux new-session -d -s "c-8-2-28-6" bash bin/models/coarse/crop8/8_2_28_6.sh
-tmux new-session -d -s "c-8-2-28-8" bash bin/models/coarse/crop8/8_2_28_8.sh
 tmux new-session -d -s "c-8-2-28-10" bash bin/models/coarse/crop8/8_2_28_10.sh
